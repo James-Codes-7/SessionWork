1d52b15060012513a5c36df877d83d7798698d81
James-Codes-7 <110839973+James-Codes-7@users.noreply.github.com>
2022-11-08 20:11:08 +0530

    Add files via upload

--- RajeeSanTask/CompressString.java
+++ RajeeSanTask/CompressString.java
@@ -17,13 +17,21 @@ public class CompressString {
     private void programPage() {
         System.out.println("Enter the String");
         String word = scanner.next(), temp = "";
-        int size = word.length(), i = 0, count = 0;
-        for (i = 0; i < size; i++) {
-            temp += word.charAt(i);
-            count = word.lastIndexOf(word.charAt(i));
-            if (i != count) temp += (count - i + 1);
-            i = count;
+        int size = word.length(), i = 0, count = 1;
+        temp += word.charAt(0);
+        for (i = 1; i < size; ) {
+            if (word.charAt(i - 1) != word.charAt(i)) {
+                if (count != 1)
+                    temp += "" + count;
+                temp += word.charAt(i);
+                count = 1;
+                i++;
+            } else {
+                count++;
+                i++;
+            }
         }
+        if (count > 1) temp += "" + count;
         System.out.println(temp);
     }
 }
